1e03bdce65718ff9a86bfa4734b7bbdce45e70a9
Leonardo Model <modelleonardo@outlook.com>
2022-10-07 20:47:31 -0300

    diagonal negativos

---
+++ C/diagonal_negativos.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, cont;
+
+    printf("Qual a ordem da matriz? ");
+    scanf("%d", &n);
+
+    int mat[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Elemento [%d,%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("DIAGONAL PRINCIPAL:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", mat[i][i]);
+    }
+
+    cont = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j=0; j<n; j++) {
+            if(mat[i][j] < 0) {
+                cont++;
+            }
+        }
+    }
+
+    printf("\nQUANTIDADE DE NEGATIVOS = %d\n", cont);
+
+    return 0;
+}
